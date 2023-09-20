45fa3a18a596d4744fc3ef10084e9f4ab3275a55
Ruby-abym <abymemp47@gmail.com>
2023-09-20 16:23:24 +0530

    delete file

--- src/pages/disclaimer/index.tsx
+++ src/pages/disclaimer/index.tsx
@@ -76,7 +76,7 @@ const Disclaimer: NextPage<MyPageProps> = ({seoData})=> {
     )
 }
 export const getStaticProps = async ({ locale }:{locale: string}) => {
-    let Slug = ROUTE.DISCLAIMER?.replace("/", "");
+    let Slug = ROUTE.DISCLAIMER?.replace("/en", "");
     const data: any = await Api.post(Url.seoDetail, { Slug: Slug });
     return {
       props: {
