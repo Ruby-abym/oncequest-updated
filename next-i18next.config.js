e4d87dbe108d9b6d1ea406f6576e5618842dfcfb
Ruby-abym <abymemp47@gmail.com>
2023-09-21 18:02:25 +0530

    offers

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,8 @@
-const path = require("path")
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "hi","ta","kn","bn"],
-  
-    returnNull: false,
-    
-    
   },
-  reloadOnPrender:true,
-  ns:['common']
+ 
 };
