34edbffb668b8a339f40dda7289476c9dced6c8f
Ruby-abym <abymemp47@gmail.com>
2023-09-22 17:25:03 +0530

    serversideprops

--- src/pages/terms-condition/index.tsx
+++ src/pages/terms-condition/index.tsx
@@ -1053,12 +1053,12 @@ const TermsAndConditon : NextPage<MyPageProps> = ({seoData})=>  {
     </React.Fragment>
   );
 };
-export const getStaticProps= async ({locale}:{locale:string}) => {
-    // let Slug = ROUTE.TERMSCONDITION?.replace("/", "");
-    // const data: any = await Api.post(Url.seoDetail, { Slug: Slug });
+export const getServerSideProps= async ({locale}:{locale:string}) => {
+    let Slug = ROUTE.TERMSCONDITION?.replace("/", "");
+    const data: any = await Api.post(Url.seoDetail, { Slug: Slug });
     return {
       props: {
-        // seoData: data?.Result?.Details || {},
+        seoData: data?.Result?.Details || {},
       ...(await serverSideTranslations(locale, ["common"])),
       },
     };
