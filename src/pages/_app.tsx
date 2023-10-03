dde7bba4b91548d3d9a25172426ffb718791b412
Ruby-abym <abymemp47@gmail.com>
2023-10-03 11:45:04 +0530

    google tag manager added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { appWithTranslation } from "next-i18next";
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps, NextWebVitalsMetric } from "next/app";
 import { Provider } from "react-redux";
 import Layout from "@/Component/Common/Layout";
@@ -13,6 +14,17 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+    <Head>
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+       })(window,document,'script','dataLayer','GTM-MG7ZRNB4');
+      `}
+    </Script>
+    </Head>
       <DefaultSeo
         title="Best Pathology Labs India | Diagnostic Centre | Blood Testing Home Collection - Oncquest Labs"
         description="India's Super Specialized Laboratories Network | Oncquest Laboratories is the best/leading pathology labs and diagnostics centre in India. We provide Blood test, Cancer testing, top Diagnostic Centre for Health check-up services at home in Delhi NCR, India. Blood testing sample collection is also available at home. Oncquest Labs is easy to use and convenient for patients to check their blood test report online."
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,4 @@
-
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
- } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 class MyDocument extends Document {
@@ -12,15 +6,43 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+        <script
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MG7ZRNB4');`,
+    }}
+  />
+       
         </Head>
         <body>
+      
           <Main />
           <NextScript />
-         
-    <Script src="./assets/js/jquery-3.4.1.min.js" crossOrigin="anonymous" type="text/javascript" strategy="beforeInteractive"></Script>
-    <Script src="./assets/js/popper.min.js" crossOrigin="anonymous" type="text/javascript" strategy="beforeInteractive"></Script>
-    <Script src="./assets/js/bootstrap.min.js" type="text/javascript" strategy="beforeInteractive"></Script>
-    
+          <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG7ZRNB4" height="0" width="0" style="display: none; visibility: hidden;" />`,
+    }}
+  />
+          <Script
+            src="./assets/js/jquery-3.4.1.min.js"
+            crossOrigin="anonymous"
+            type="text/javascript"
+            strategy="beforeInteractive"
+          ></Script>
+          <Script
+            src="./assets/js/popper.min.js"
+            crossOrigin="anonymous"
+            type="text/javascript"
+            strategy="beforeInteractive"
+          ></Script>
+          <Script
+            src="./assets/js/bootstrap.min.js"
+            type="text/javascript"
+            strategy="beforeInteractive"
+          ></Script>
         </body>
       </Html>
     );
